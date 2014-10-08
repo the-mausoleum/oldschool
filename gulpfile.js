4340473d2ec4a698644f83f24af24059e7685fca
Marshall Bowers <elliott.codes@gmail.com>
2014-10-08 12:45:28 -0400

    Add Gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,29 @@
 'use strict';
 
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var beautify = require('gulp-beautify');
 
-gulp.task('default', []);
+var path = 'quest-checker/*.js';
+
+gulp.task('lint', function () {
+    return gulp.src(path)
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('beautify', function () {
+    gulp.src(path)
+        .pipe(beautify({ indentSize: 4 }))
+        .pipe(gulp.dest('js'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(path, ['lint', 'beautify']);
+});
+
+gulp.task('default', [
+    'lint',
+    'beautify',
+    'watch'
+]);
