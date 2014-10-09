05141c11ff6c7559f04447818e9cd131e4a58d83
Marshall Bowers <elliott.codes@gmail.com>
2014-10-09 14:16:03 -0400

    Add player lookup

--- app/quests/quests.js
+++ app/quests/quests.js
@@ -1,12 +1,8 @@
 'use strict';
 
-$.get('http://localhost:3000/stats/Max+Deviant', function (data) {
-    var skills = ['overall', 'attack', 'defence', 'strength', 'constitution', 'ranged', 'prayer', 'magic', 'cooking', 'woodcutting', 'fletching', 'fishing', 'firemaking', 'crafting', 'smithing', 'mining', 'herblore', 'agility', 'thieving', 'slayer', 'farming', 'runecrafting', 'hunter', 'construction'];
+var player = '';
 
-    for (var i in skills) {
-        $('.' + skills[i]).html('<span class="level">' + data[skills[i]].level + '</span>');
-    }
-});
+lookup();
 
 $.get('../../quest-checker/quests.json', function (data) {
     console.log(data);
@@ -29,4 +25,16 @@ function generateQuestList(quests) {
     }
 
     console.log(built)    
+}
+
+function lookup() {
+    player = $('input[name="player"]').val();
+
+    $.get('http://localhost:3000/stats/' + player, function (data) {
+        var skills = ['overall', 'attack', 'defence', 'strength', 'constitution', 'ranged', 'prayer', 'magic', 'cooking', 'woodcutting', 'fletching', 'fishing', 'firemaking', 'crafting', 'smithing', 'mining', 'herblore', 'agility', 'thieving', 'slayer', 'farming', 'runecrafting', 'hunter', 'construction'];
+
+        for (var i in skills) {
+            $('.' + skills[i]).html('<span class="level">' + data[skills[i]].level + '</span>');
+        }
+    });
 }
