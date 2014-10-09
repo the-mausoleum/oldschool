6dd967c43339277bb0e999c6ae91860dc793928a
Marshall Bowers <elliott.codes@gmail.com>
2014-10-09 17:25:51 -0400

    Create completable quest list

--- app/js/controllers/quests.js
+++ app/js/controllers/quests.js
@@ -1,6 +1,19 @@
 'use strict';
 
 angular.module('OldSchool')
-    .controller('QuestsCtrl', ['$scope', function ($scope) {
-        console.log('Quests');
+    .controller('QuestsCtrl', ['$scope', '$http', function ($scope, $http) {
+        $scope.quests = [];
+        $scope.completed = {};
+
+        $http.get('../quest-checker/quests.json')
+            .success(function (data, status, headers, config) {
+                $scope.quests = data;
+            })
+            .error(function (data, status, headers, config) {
+                console.log(data);
+            });
+
+        $scope.updateQuestList = function (slug) {
+            
+        };
     }]);
