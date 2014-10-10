8eaae8000bc5d5d401e181dbaeb1b0ba671dc765
Marshall Bowers <elliott.codes@gmail.com>
2014-10-10 10:56:59 -0400

    Replace missing code

---
+++ app/js/filters/numbers.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('OldSchool').filter('exp', function () {
+    return function (num) {
+        num = num || '';
+
+        return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    };
+});
