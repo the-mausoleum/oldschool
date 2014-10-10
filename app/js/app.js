121ed9128d9ae6a87557dcfe8e9f52e82aec8005
Marshall Bowers <elliott.codes@gmail.com>
2014-10-10 10:50:16 -0400

    Format app.js
    Does not seem to get caught by Gulp

--- app/js/app.js
+++ app/js/app.js
@@ -2,37 +2,32 @@
 
 angular.module('OldSchool', ['ui.router']);
 
-angular.module('OldSchool')
-    .config(function ($stateProvider, $urlRouterProvider) {
-        $stateProvider
-            .state('stats', {
-                url: '/stats',
-                views: {
-                    '': {
-                        controller: 'StatsCtrl',
-                        templateUrl: 'partials/stats.html'
-                    }
-                }
-            })
-            .state('quests', {
-                url: '/quests',
-                views: {
-                    '': {
-                        controller: 'QuestsCtrl',
-                        templateUrl: 'partials/quests.html'
-                    }
-                }
-            })
-            .state('quests-details', {
-                url: '/quests/:slug',
-                views: {
-                    '': {
-                        controller: 'QuestsCtrl',
-                        templateUrl: 'partials/quests-details.html'
-                    }
-                }
-            });
-
-        $urlRouterProvider
-            .otherwise('404');
+angular.module('OldSchool').config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('stats', {
+        url: '/stats',
+        views: {
+            '': {
+                controller: 'StatsCtrl',
+                templateUrl: 'partials/stats.html'
+            }
+        }
+    }).state('quests', {
+        url: '/quests',
+        views: {
+            '': {
+                controller: 'QuestsCtrl',
+                templateUrl: 'partials/quests.html'
+            }
+        }
+    }).state('quests-details', {
+        url: '/quests/:slug',
+        views: {
+            '': {
+                controller: 'QuestsCtrl',
+                templateUrl: 'partials/quests-details.html'
+            }
+        }
     });
+
+    $urlRouterProvider.otherwise('404');
+});
