a29fb57cd871b04cdb282e3f54ee2e6dd51d65dc
Marshall Bowers <elliott.codes@gmail.com>
2014-10-13 08:57:19 -0400

    Indicate recommended quests

--- app/js/controllers/quests.js
+++ app/js/controllers/quests.js
@@ -2,28 +2,50 @@
 
 angular.module('OldSchool').controller('QuestsCtrl', ['$scope', '$stateParams', '$http', 'grep', 'Quests', function ($scope, $stateParams, $http, grep, Quests) {
     $scope.quests = [];
-    $scope.completed = {};
+    $scope.recommended = [];
+
+    $scope.viewOptions = {
+        showCompleted: true
+    };
 
     $scope.slug = $stateParams.slug;
 
-    $http.get('../data/quests.json').success(function (data, status, headers, config) {
-        $scope.quests = data;
+    $scope.player = 'Max Deviant';
 
-        if ($stateParams.slug) {
-            $scope.quest = grep($scope.quests, 'slug', $stateParams.slug);
-            $scope.subquests = {};
+    $http.get('http://localhost:3000/stats/' + $scope.player).success(function (data, status, headers, config) {
+        $scope.stats = data;
 
-            for (var i in $scope.quest.requirements.quests) {
-                var slug = $scope.quest.requirements.quests[i];
+        $http.get('../data/quests.json').success(function (data, status, headers, config) {
+            $scope.quests = data;
 
-                $scope.subquests[slug] = grep($scope.quests, 'slug', slug);
+            if ($stateParams.slug) {
+                $scope.quest = grep($scope.quests, 'slug', $stateParams.slug);
+                $scope.subquests = {};
+
+                for (var i in $scope.quest.requirements.quests) {
+                    var slug = $scope.quest.requirements.quests[i];
+
+                    $scope.subquests[slug] = grep($scope.quests, 'slug', slug);
+                }
             }
-        }
+
+            $scope.tracker = new Quests($scope.quests, $scope.stats);
+        }).error(function (data, status, headers, config) {
+            console.log(data);
+        });
     }).error(function (data, status, headers, config) {
         console.log(data);
     });
 
     $scope.updateQuestList = function (slug) {
+        $scope.recommended = $scope.tracker.recommendNext();
+    };
 
+    $scope.isRecommended = function (slug) {
+        return $scope.recommended.indexOf(slug) !== -1;
+    };
+
+    $scope.hideQuest = function (slug) {
+        return $scope.tracker.completed.list[slug] && !$scope.viewOptions.showCompleted;
     };
 }]);
